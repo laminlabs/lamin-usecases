7ed05f231aae20e6bd140b734bdb1191b68dd016
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-29 16:03:28 +0200

    ♻️ Refactor project flow (#55)

--- docs/data-flow.md
+++ docs/data-flow.md
@@ -1,7 +1,5 @@
 # Data flow
 
-<meta http-equiv="Refresh" content="0; url=./birds-eye" />
-
 ```{toctree}
 :maxdepth: 1
 
--- noxfile.py
+++ noxfile.py
@@ -21,7 +21,7 @@ GROUPS["by_registry"] = [
     "enrichr.ipynb",
     # these could be bucketed elsewhere
     "analysis-flow.ipynb",
-    "birds-eye.ipynb",
+    "project-flow.ipynb",
 ]
 
 
@@ -51,7 +51,8 @@ def install(session, group):
     session.run(
         "pip",
         "install",
-        f"lamindb[dev,bionty{extras}] @ git+https://github.com/laminlabs/lamindb",
+        f"lamindb[dev,bionty{extras}] @"
+        " git+https://github.com/laminlabs/lamindb@release",
     )
 
 
