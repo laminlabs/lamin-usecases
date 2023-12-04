a1039f2779f8b66f8acc7e3ad6b39cebd214fa8a
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-12-04 14:42:14 -0500

    📝 Added celltypist and pathway analyses (#84)

--- lamin_usecases/_datasets.py
+++ lamin_usecases/_datasets.py
@@ -4,7 +4,7 @@ DATASETDIR = Path(__file__).parent.resolve() / "data/"
 DATASETDIR.mkdir(exist_ok=True)
 
 
-def anndata_seurat_ifnb(preprocess: bool = True):
+def anndata_seurat_ifnb(preprocess: bool = True, populate_registries: bool = False):
     """Seurat ifnb dataset.
 
     PBMCs were split into a stimulated and control group and the stimulated group was treated with interferon beta.
@@ -20,6 +20,7 @@ def anndata_seurat_ifnb(preprocess: bool = True):
     >>> Convert("ifnb.h5seurat", "ifnb.h5ad", overwrite = T)
     """
     import anndata as ad
+    import pandas as pd
     from bionty.dev._io import s3_bionty_assets
 
     filepath = DATASETDIR / "ifnb.h5ad"
@@ -30,38 +31,61 @@ def anndata_seurat_ifnb(preprocess: bool = True):
     )
 
     adata = ad.read(filepath)
+    # from https://satijalab.org/seurat/archive/v3.2/immune_alignment.html
+    anno_mapper = {
+        "0": "CD14 Mono",
+        "1": "CD4 Naive T",
+        "2": "CD4 Memory T",
+        "3": "CD16 Mono",
+        "4": "B",
+        "5": "CD8 T",
+        "6": "NK",
+        "7": "T activated",
+        "8": "DC",
+        "9": "B Activated",
+        "10": "Mk",
+        "11": "pDC",
+        "12": "Eryth",
+    }
+    adata.obs["seurat_annotations"] = (
+        adata.obs["seurat_annotations"].astype(str).map(anno_mapper)
+    )
+    adata.var.rename(columns={"features": "symbol"}, inplace=True)
+    adata.raw.var.rename(columns={"_index": "symbol"}, inplace=True)
+    adata.obs.drop(
+        columns=["orig.ident", "nCount_RNA", "nFeature_RNA", "seurat_annotations"],
+        inplace=True,
+    )
+    # set STIM as class 0, CTRL as class 1, to make categorical
+    adata.obs["stim"] = pd.Categorical(
+        adata.obs["stim"], categories=["STIM", "CTRL"], ordered=True
+    )
+    indices = adata.obs.sort_values(["stim"]).index
+    adata = adata[indices, :].copy()
     if preprocess:
-        import pandas as pd
         import scanpy as sc
-
-        # from https://satijalab.org/seurat/archive/v3.2/immune_alignment.html
-        anno_mapper = {
-            "0": "CD14 Mono",
-            "1": "CD4 Naive T",
-            "2": "CD4 Memory T",
-            "3": "CD16 Mono",
-            "4": "B",
-            "5": "CD8 T",
-            "6": "NK",
-            "7": "T activated",
-            "8": "DC",
-            "9": "B Activated",
-            "10": "Mk",
-            "11": "pDC",
-            "12": "Eryth",
-        }
-        adata.obs["seurat_annotations"] = (
-            adata.obs["seurat_annotations"].astype(str).map(anno_mapper)
-        )
 
         sc.pp.normalize_total(adata)
         sc.pp.log1p(adata)
 
-        # set STIM as class 0, CTRL as class 1, to make categorical
-        adata.obs["stim"] = pd.Categorical(
-            adata.obs["stim"], categories=["STIM", "CTRL"], ordered=True
-        )
-        indices = adata.obs.sort_values(["seurat_annotations", "stim"]).index
-        adata = adata[indices, :].copy()
-        del adata.raw  # gives issues when saving
+    if populate_registries:
+        import lamindb as ln
+        import lnschema_bionty as lb
+
+        lb.settings.organism = "human"
+
+        verbosity = ln.settings.verbosity
+        ln.settings.verbosity = 0
+        adata.var.index = lb.Gene.standardize(adata.var.index)
+        validated = lb.Gene.validate(adata.var.index)
+        adata = adata[:, validated].copy()
+        adata.raw = adata.raw[:, validated].to_adata()
+        adata.raw.var.index = adata.var.index
+        duplicated = adata.var.index.duplicated()
+        adata = adata[:, ~duplicated].copy()
+        adata.raw = adata.raw[:, ~duplicated].to_adata()
+        adata.raw.var.index = adata.var.index
+        ln.save(lb.Gene.from_values(adata.var.index))
+        ln.settings.verbosity = verbosity
+
     return adata
