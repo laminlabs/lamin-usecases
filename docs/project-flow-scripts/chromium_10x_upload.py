e347ebc19cd88272724f287fd07b8be3d9aa3973
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-04-22 12:01:37 +0200

    ♻️ Refactor project flow

---
+++ docs/project-flow-scripts/chromium_10x_upload.py
@@ -0,0 +1,17 @@
+import lamindb as ln
+
+
+ln.setup.login("testuser1")
+
+ln.settings.transform.stem_uid = "qCJPkOuZAi9q"
+ln.settings.transform.version = "1"
+ln.track()
+
+# register output files of the sequencer
+upload_dir = ln.core.datasets.dir_scrnaseq_cellranger(
+    "perturbseq", basedir=ln.settings.storage, output_only=False
+)
+ln.Artifact(upload_dir.parent / "fastq/perturbseq_R1_001.fastq.gz").save()
+ln.Artifact(upload_dir.parent / "fastq/perturbseq_R2_001.fastq.gz").save()
+
+ln.finish()
