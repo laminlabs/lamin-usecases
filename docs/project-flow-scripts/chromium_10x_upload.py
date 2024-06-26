900df36b94806590d80f63be1623805940e74da3
Alex Wolf <f.alexander.wolf@gmail.com>
2024-06-26 18:21:26 +0200

    ⬆️ Fix for lamindb 0.74.1

--- docs/project-flow-scripts/chromium_10x_upload.py
+++ docs/project-flow-scripts/chromium_10x_upload.py
@@ -9,7 +9,7 @@ ln.track()
 
 # register output files of the sequencer
 upload_dir = ln.core.datasets.dir_scrnaseq_cellranger(
-    "perturbseq", basedir=ln.settings.storage, output_only=False
+    "perturbseq", basedir=ln.settings.storage.root, output_only=False
 )
 ln.Artifact(upload_dir.parent / "fastq/perturbseq_R1_001.fastq.gz").save()
 ln.Artifact(upload_dir.parent / "fastq/perturbseq_R2_001.fastq.gz").save()
--- docs/project-flow-scripts/postprocess_cellranger.py
+++ docs/project-flow-scripts/postprocess_cellranger.py
@@ -10,6 +10,6 @@ output_artifacts = ln.Artifact.filter(
     key__startswith="perturbseq/filtered_feature_bc_matrix"
 ).all()
 input_artifacts = [f.cache() for f in output_artifacts]
-output_path = ln.core.datasets.schmidt22_perturbseq(basedir=ln.settings.storage)
+output_path = ln.core.datasets.schmidt22_perturbseq(basedir=ln.settings.storage.root)
 output_file = ln.Artifact(output_path, description="perturbseq counts")
 output_file.save()
