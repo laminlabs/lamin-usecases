192ca2291121b83f8a8e8878d56674f785e3b2c5
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-08-02 16:54:54 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -83,10 +83,6 @@ def install(session, group):
         extras += ",aws,jupyter"
     elif group == "docs":
         extras += ""
-    session.run(*"uv pip install --system .[dev]".split())
-    session.run(*"git clone https://github.com/laminlabs/lamindb --recursive".split())
-    with session.chdir("./lamindb"):
-        session.run(*"git switch fix-source".split())
     if IS_PR:
         session.run(
             "uv",
@@ -99,6 +95,10 @@ def install(session, group):
             "./lamindb/sub/lamin-cli",
             "./lamindb/sub/bionty",
         )
+    session.run(*"git clone https://github.com/laminlabs/lamindb --recursive".split())
+    with session.chdir("./lamindb"):
+        session.run(*"git switch fix-source".split())
+    session.run(*"uv pip install --system .[dev]".split())
     session.run(
         "uv",
         "pip",
