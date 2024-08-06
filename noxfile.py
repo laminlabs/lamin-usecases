53d657aa8408d668a9066c196cb355dd33ad1e99
Alex Wolf <f.alexander.wolf@gmail.com>
2024-08-06 19:18:06 +0200

    💚 Run on lamindb release

--- noxfile.py
+++ noxfile.py
@@ -94,7 +94,7 @@ def install(session, group):
         session, "uv pip install --system ipywidgets"
     )  # needed to silence the jupyter warning
     run(session, "uv pip install --system .[dev]")
-    branch = "main" if IS_PR else "main"  # point back to "release"
+    branch = "main" if IS_PR else "release"  # point back to "release"
     install_lamindb(session, branch=branch, extras=extras)
 
 
