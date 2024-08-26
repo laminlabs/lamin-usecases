41caa42a796bec78cebbbe0b39a67a6563a03be7
Koncopd <koncopd@gmail.com>
2024-08-26 18:40:57 +0200

    try installing pytometry first

--- noxfile.py
+++ noxfile.py
@@ -74,10 +74,8 @@ def install(session, group):
     extras = "bionty"
     if group == "by_datatype":
         extras += ",fcs,jupyter"
-        run(session, "uv pip install --system scanpy")
-        run(
-            session, "uv pip install --system pytometry dask[dataframe]"
-        )  # needed by datashader
+        run(session, "uv pip install --system pytometry")  # includes scanpy
+        run(session, "uv pip install --system dask[dataframe]")  # needed by datashader
         run(session, "uv pip install --system mudata")
         run(session, "uv pip install --system torch")
         run(session, "uv pip install --system tiledbsoma")
