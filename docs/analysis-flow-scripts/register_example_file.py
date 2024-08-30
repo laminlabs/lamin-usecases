f4e5b6db194a13f0fa01ce8981ce4e47da3395ec
zethson <lukas.heumos@posteo.net>
2024-08-30 13:01:56 +0200

    :bug: Fix analysis-flow registration
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- docs/analysis-flow-scripts/register_example_file.py
+++ docs/analysis-flow-scripts/register_example_file.py
@@ -2,7 +2,7 @@ import lamindb as ln
 import bionty as bt
 
 
-ln.context.uid = "K4wsS5DTYdFp0000"
+ln.context.uid = "QkQJXJlOTsyM0000"
 ln.context.track()
 
 # an example dataset that has a few cell type, tissue and disease annotations
@@ -14,11 +14,13 @@ curate = ln.Curate.from_anndata(
     var_index=bt.Gene.ensembl_gene_id,
     categoricals={
         "cell_type": bt.CellType.name,
+        "cell_type_id": bt.CellType.ontology_id,
         "tissue": bt.Tissue.name,
         "disease": bt.Disease.name,
     },
     organism="human",
 )
+curate.add_validated_from_var_index()
 curate.add_validated_from("all")
 curate.add_new_from("cell_type")
 curate.validate()
