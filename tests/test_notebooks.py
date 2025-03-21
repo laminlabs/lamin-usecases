c033d0745c4e6d7f4e4743e9be23e189180c6e6b
Lukas Heumos <lukas.heumos@posteo.net>
2025-03-21 14:30:03 +0100

    🎨 Refactor
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -10,19 +10,20 @@ from noxfile import GROUPS
 DOCS = Path(__file__).parents[1] / "docs/"
 
 
-def test_by_datatype():
-    for filename in GROUPS["by_datatype"]:
-        print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+def _test_group(group_name: str) -> None:
+    for filename in GROUPS[group_name]:
+        notebook_path = filename + ".ipynb"
+        print(notebook_path)
+        test.execute_notebooks(DOCS / notebook_path, write=True)
 
 
-def test_by_registry():
-    for filename in GROUPS["by_registry"]:
-        print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+def test_by_datatype() -> None:
+    _test_group("by_datatype")
 
 
-def test_by_ontology():
-    for filename in GROUPS["by_ontology"]:
-        print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+def test_by_registry() -> None:
+    _test_group("by_registry")
+
+
+def test_by_ontology() -> None:
+    _test_group("by_ontology")
