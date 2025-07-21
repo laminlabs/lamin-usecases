c343c94f44ebcf78762218d96652f90cab71124b
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-21 12:16:30 +0200

    ♻️ Add print_outputs True to facilitate debugging

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -13,28 +13,28 @@ DOCS = Path(__file__).parents[1] / "docs/"
 def test_by_datatype():
     for filename in GROUPS["by_datatype"]:
         print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+        test.execute_notebooks(DOCS / filename, write=True, print_outputs=True)
 
 
 def test_by_datatype_spatial():
     for filename in GROUPS["by_datatype_spatial"]:
         print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+        test.execute_notebooks(DOCS / filename, write=True, print_outputs=True)
 
 
 def test_by_datatype_sc_imaging():
     for filename in GROUPS["by_datatype_sc_imaging"]:
         print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+        test.execute_notebooks(DOCS / filename, write=True, print_outputs=True)
 
 
 def test_by_registry():
     for filename in GROUPS["by_registry"]:
         print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+        test.execute_notebooks(DOCS / filename, write=True, print_outputs=True)
 
 
 def test_by_ontology():
     for filename in GROUPS["by_ontology"]:
         print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+        test.execute_notebooks(DOCS / filename, write=True, print_outputs=True)
