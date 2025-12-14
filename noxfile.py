a7173ce787d9a9b6cb559c6dd4eef7e3fde83573
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-12-14 20:32:41 +0100

    👷 Point back to main

--- noxfile.py
+++ noxfile.py
@@ -138,7 +138,7 @@ def install(session, group):
         session, "uv pip install --system ipywidgets"
     )  # needed to silence the jupyter warning
     run(session, "uv pip install --system .[dev]")
-    branch = "update-bionty-organism" if IS_PR else "main"
+    branch = "main" if IS_PR else "release"
     install_lamindb(session, branch=branch, extras=extras)
 
 
